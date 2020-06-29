6186b9452279ff6d707b76580661e5db112243d8
oberntorp <oberntorp@hotmail.com>
2020-06-29 19:17:07 +0200

    From simple jsx to another component with data passed to it

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I am a react app!</h1>
+        <p>It is working tjoho!</p>
+        <Person name="Oskar" age="28"/>
+        <Person name="Eliot" age="28">My hobbies: Computers</Person>
+        <Person name="Max" age="28"/>
+
       </div>
     );
   }
