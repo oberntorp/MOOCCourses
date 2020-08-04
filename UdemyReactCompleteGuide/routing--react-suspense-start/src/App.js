cb8d80133dda2f9c4f9fbe4b9ad736b3c54940db
oberntorp <oberntorp@hotmail.com>
2020-08-04 13:00:42 +0200

    - Learning about lazyloading Components with React.lazy

---
+++ UdemyReactCompleteGuide/routing--react-suspense-start/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component, Suspense } from 'react';
+//import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+
+// import Posts from './containers/Posts';
+import User from './containers/User';
+//import Welcome from './containers/Welcome';
+
+const Posts = React.lazy(() => import('./containers/Posts'));
+
+class App extends Component {
+  state = {
+    showPages: false
+  }
+
+  swithModeHandler = () =>{
+    this.setState({showPages: !this.state.showPages});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+              <button onClick={this.swithModeHandler}>Toggle mode</button>
+              { this.state.showPages ? <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense> : <User/>}
+      </React.Fragment>
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     <Route path="/posts" render={() => <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>} />
+      //   </React.Fragment>
+      // </BrowserRouter>
+    );
+  }
+}
+
+export default App;
