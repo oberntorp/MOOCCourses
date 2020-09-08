ae74a5fe4da71797845a88bee87c4ab8d211149f
oberntorp <oberntorp@hotmail.com>
2020-09-08 16:18:37 +0200

    - Configuring Webpack - When npm start I get an error

---
+++ UdemyReactCompleteGuide/WorkingWithWebpack/src/containers/Users.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Users extends Component{
+    render(){
+        return (
+            <div>
+                <h1>The Pizza</h1>
+                <p>Awsome users on this course</p>
+            </div>
+        );
+    }
+}
+
+export default Pizza;
