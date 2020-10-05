bd39ec6b48d85dbf29cc02bd7794edb3deb786de
oberntorp <oberntorp@hotmail.com>
2020-10-05 13:51:49 +0200

    - Implementerat Sort enligt wikter, baserat på sammavikt

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(OrderWeight("103 123 4444 99 2000"));
+            Console.WriteLine(OrderWeight("56 65 74 100"));
+            Console.WriteLine(OrderWeight("56 65 74 100 99 68 86 180 90"));
+            Console.WriteLine(OrderWeight("103 4 123 4444 99 2000"));
+            Console.WriteLine(OrderWeight("4 103 310 33 6 24"));
+            Console.WriteLine(OrderWeight("2000 10003 11 11"));
             Console.ReadKey();
         }
 
@@ -18,6 +23,13 @@ namespace ConsoleApp1
         {
             List<string> weights = new List<string>();
             string[] numbers = strng.Split(' ');
+            GetWeights(weights, numbers);
+
+            return (strng.Length == 0) ? "" : SortNumbersAccordingToWeight(numbers, weights);
+        }
+
+        private static void GetWeights(List<string> weights, string[] numbers)
+        {
             foreach (string number in numbers)
             {
                 int lengthOfNuber = number.Length;
@@ -28,18 +40,23 @@ namespace ConsoleApp1
                 }
                 weights.Add(sum.ToString());
             }
-
-            return (strng.Length == 0) ? "" : SortNumbersAccordingToWeight(numbers, weights);
         }
 
         public static string SortNumbersAccordingToWeight(string[] numbers, List<string> weights)
         {
-            List<string> sortedWeights = new List<string>();
-
-            for (int i = 0; i < numbers.Length; i++)
+            SortNumbersDifferentWeight(numbers, weights);
+            if(weights.Any(x => x == x))
             {
-                sortedWeights.Add(weights[i]);
+                SortWeights(weights);
+                SortNumbersOfSameWeight(numbers, weights);
             }
+
+            return String.Join(" ", numbers);
+
+        }
+
+        private static void SortNumbersDifferentWeight(string[] numbers, List<string> sortedWeights)
+        {
             for (int pass = 0; pass < sortedWeights.Count; pass++)
             {
                 for (int j = pass; j < sortedWeights.Count; j++)
@@ -54,9 +71,43 @@ namespace ConsoleApp1
                     }
                 }
             }
+        }
 
-            return String.Join(" ", numbers);
+        private static void SortWeights(List<string> sortedWeights)
+        {
+            for (int pass = 0; pass < sortedWeights.Count; pass++)
+            {
+                for (int j = pass; j < sortedWeights.Count; j++)
+                {
+                    if (int.Parse(sortedWeights[j]) < int.Parse(sortedWeights[pass]))
+                    {
+                        string temp = sortedWeights[pass];
 
+                        sortedWeights[pass] = sortedWeights[j];
+                        sortedWeights[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private static void SortNumbersOfSameWeight(string[] numbers, List<string> sortedWeights)
+        {
+            for (int pass = 0; pass < sortedWeights.Count; pass++)
+            {
+                for (int j = pass; j < sortedWeights.Count; j++)
+                {
+                    if (int.Parse(sortedWeights[j]) == int.Parse(sortedWeights[pass]))
+                    {
+                        if (numbers[j].Length < numbers[pass].Length)
+                        {
+                            string temp = numbers[pass];
+
+                            numbers[pass] = numbers[j];
+                            numbers[j] = temp;
+                        }
+                    }
+                }
+            }
         }
     }
 }
