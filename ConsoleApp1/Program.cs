f55a0bc0ae9ab3401b9e7f99c646db58fa7aed90
oberntorp <oberntorp@hotmail.com>
2020-10-08 15:13:40 +0200

    - Additional changes to sorting with sameWeight - Refactorings to swappingElements

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,103 +11,101 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(OrderWeight("56 65 74 100"));
-            Console.WriteLine(OrderWeight("56 65 74 100 99 68 86 180 90"));
-            Console.WriteLine(OrderWeight("103 4 123 4444 99 2000"));
-            Console.WriteLine(OrderWeight("4 103 310 33 6 24"));
-            Console.WriteLine(OrderWeight("2000 10003 11 11"));
+            //Console.WriteLine(OrderWeight("56 74 100"));
+            //Console.WriteLine(OrderWeight("56 65 74 100 99 68 86 180 90"));
+            //Console.WriteLine(OrderWeight("103 4 123 4444 99 2000"));
+            //Console.WriteLine(OrderWeight("4 103 310 33 6 24"));
+            Console.WriteLine(OrderWeight("2000 11 1003 11 9999 22 123"));
             Console.ReadKey();
         }
 
         public static string OrderWeight(string strng)
         {
-            List<string> weights = new List<string>();
             string[] numbers = strng.Split(' ');
+            string[] weights = new string[numbers.Length];
             GetWeights(weights, numbers);
 
             return (strng.Length == 0) ? "" : SortNumbersAccordingToWeight(numbers, weights);
         }
 
-        private static void GetWeights(List<string> weights, string[] numbers)
+        private static void GetWeights(string[] weights, string[] numbers)
         {
-            foreach (string number in numbers)
+            for(int i = 0; i < numbers.Length; i++)
             {
-                int lengthOfNuber = number.Length;
+                int lengthOfNuber = numbers[i].Length;
                 int sum = 0;
-                for (int i = 0; i < lengthOfNuber; i++)
+                for (int j = 0; j < lengthOfNuber; j++)
                 {
-                    sum += int.Parse(number[i].ToString());
+                    sum += int.Parse(numbers[i][j].ToString());
                 }
-                weights.Add(sum.ToString());
+                weights[i] = sum.ToString();
             }
         }
 
-        public static string SortNumbersAccordingToWeight(string[] numbers, List<string> weights)
+        public static string SortNumbersAccordingToWeight(string[] numbers, string[] weights)
         {
+            SortNumbersOfSameWeight(numbers, weights);
             SortNumbersDifferentWeight(numbers, weights);
-            if(weights.Any(x => x == x))
-            {
-                SortWeights(weights);
-                SortNumbersOfSameWeight(numbers, weights);
-            }
 
             return String.Join(" ", numbers);
 
         }
 
-        private static void SortNumbersDifferentWeight(string[] numbers, List<string> sortedWeights)
+        private static void SortNumbersDifferentWeight(string[] numbers, string[] weights)
         {
-            for (int pass = 0; pass < sortedWeights.Count; pass++)
+            for (int pass = 0; pass < weights.Length; pass++)
             {
-                for (int j = pass; j < sortedWeights.Count; j++)
+                for (int j = pass; j < weights.Length; j++)
                 {
-                    if (int.Parse(sortedWeights[j]) < int.Parse(sortedWeights[pass]))
+                    if (int.Parse(weights[pass]) > int.Parse(weights[j]))
                     {
-                        string temp = numbers[pass];
-
-                        numbers[pass] = numbers[j];
-                        numbers[j] = temp;
-
+                        Swap(numbers, pass, j);
                     }
                 }
             }
         }
 
-        private static void SortWeights(List<string> sortedWeights)
+        private static void SortNumbersOfSameWeight(string[] numbers, string[] weights)
         {
-            for (int pass = 0; pass < sortedWeights.Count; pass++)
+            for (int pass = 0; pass < weights.Length; pass++)
             {
-                for (int j = pass; j < sortedWeights.Count; j++)
+                for (int j = pass; j < weights.Length; j++)
                 {
-                    if (int.Parse(sortedWeights[j]) < int.Parse(sortedWeights[pass]))
+                    if (int.Parse(weights[j]) == int.Parse(weights[pass]))
                     {
-                        string temp = sortedWeights[pass];
-
-                        sortedWeights[pass] = sortedWeights[j];
-                        sortedWeights[j] = temp;
-                    }
-                }
-            }
-        }
-
-        private static void SortNumbersOfSameWeight(string[] numbers, List<string> sortedWeights)
-        {
-            for (int pass = 0; pass < sortedWeights.Count; pass++)
-            {
-                for (int j = pass; j < sortedWeights.Count; j++)
-                {
-                    if (int.Parse(sortedWeights[j]) == int.Parse(sortedWeights[pass]))
-                    {
-                        if (numbers[j].Length < numbers[pass].Length)
+                        if (numbers[j].Length < numbers[pass].Length && IsNumberJBiggerThanNumberPass(numbers[j], numbers[pass]))
                         {
-                            string temp = numbers[pass];
-
-                            numbers[pass] = numbers[j];
-                            numbers[j] = temp;
+                            Swap(numbers, pass, j);
                         }
+                        else if(int.Parse(numbers[pass]) > int.Parse(numbers[j]))
+                        {
+                            Swap(numbers, pass, j);
+                        }
+
                     }
                 }
             }
+        }
+
+        private static bool IsNumberJBiggerThanNumberPass(string later, string earlier)
+        {
+            for(int i = 0; i < later.Length; i++)
+            {
+                if(earlier[i] > later[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void Swap(string[] arraySwapExecutedOn, int earlier, int later)
+        {
+            string temp = arraySwapExecutedOn[earlier];
+
+            arraySwapExecutedOn[earlier] = arraySwapExecutedOn[later];
+            arraySwapExecutedOn[later] = temp;
         }
     }
 }
