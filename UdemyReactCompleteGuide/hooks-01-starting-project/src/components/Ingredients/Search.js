5357fb698ee7ae8572652e46921800565eddf3db
oberntorp <oberntorp@hotmail.com>
2021-04-15 13:37:07 +0200

    Changes made:
    - Implementing Custom hook into search
    - Preparing for Section 48 Bonus: Replacing Redux with react hooks

--- UdemyReactCompleteGuide/hooks-01-starting-project/src/components/Ingredients/Search.js
+++ UdemyReactCompleteGuide/hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -2,39 +2,47 @@ import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
+import useHttp from '../../hooks/http';
+import ErrorModal from '../UI/ErrorModal';
 
 const Search = React.memo(props => {
   const {onFilteredIngredients} = props;
   const [enteredFilter, setEnteredFilter] = useState("");
+  const {isLoading, data, error, sendRequest, clearError} = useHttp();
   const inputRef = useRef();
+
   useEffect(() => {
     const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const queryParams = enteredFilter.length === 0 ? "" : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch(`https://myreacthooks-6c6c3-default-rtdb.firebaseio.com/ingredients.json${queryParams}`)
-          .then(response => response.json())
-          .then(responseData => {
-            const loadedIngredinets = [];
-            for (const key in responseData) {
-              loadedIngredinets.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount
-              });
-            }
-            onFilteredIngredients(loadedIngredinets);
-          });
+        sendRequest(`https://myreacthooks-6c6c3-default-rtdb.firebaseio.com/ingredients.json${queryParams}`, "get")
       }
 
       return clearTimeout(timer);
     }, 500);
-  }, [enteredFilter, onFilteredIngredients, inputRef]);
+  }, [enteredFilter, inputRef, sendRequest]);
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredinets = [];
+      for (const key in data) {
+        loadedIngredinets.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        });
+      }
+      onFilteredIngredients(loadedIngredinets);
+    }
+  }, [isLoading, error, data, onFilteredIngredients]);
 
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Searching...</span>}
           <input type="text" ref={inputRef} value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)} />
         </div>
       </Card>
