d0975e90b185a709052bb9efe56f95fef85aa2f6
oberntorp <oberntorp@hotmail.com>
2021-08-29 12:32:08 +0200

    Changes made:
    - Section 3 Completed

---
+++ UdemyReactUltimateCourceNew/01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import Card from '../UI/Card';
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      />
+      <ExpenseItem
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      />
+      <ExpenseItem
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      />
+      <ExpenseItem
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
