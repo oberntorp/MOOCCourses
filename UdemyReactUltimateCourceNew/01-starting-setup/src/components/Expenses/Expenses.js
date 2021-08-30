b809f6d7cc0cc7d32a5837b59f217e86601d5327
oberntorp <oberntorp@hotmail.com>
2021-08-30 19:17:18 +0200

    Changes made:
    - Section 4 completed

--- UdemyReactUltimateCourceNew/01-starting-setup/src/components/Expenses/Expenses.js
+++ UdemyReactUltimateCourceNew/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const yearChangedHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
+    <div>
     <Card className="expenses">
+      <ExpensesFilter selectedYear={filteredYear} onYearChanged={yearChangedHandler}/>
       <ExpenseItem
         title={props.items[0].title}
         date={props.items[0].date}
@@ -26,6 +34,7 @@ const Expenses = (props) => {
         amount={props.items[0].amount}
       />
     </Card>
+    </div>
   );
 }
 
