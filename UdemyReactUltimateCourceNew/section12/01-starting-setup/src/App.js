5fd49d88fd11eabfa8065d3d672d3cd32c06a623
oberntorp <oberntorp@hotmail.com>
2021-10-08 14:38:48 +0200

    Changes made:
    - Section 12 done.

--- UdemyReactUltimateCourceNew/section12/01-starting-setup/src/App.js
+++ UdemyReactUltimateCourceNew/section12/01-starting-setup/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useCallback } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  const [allowToggle, setAllowToggle] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
