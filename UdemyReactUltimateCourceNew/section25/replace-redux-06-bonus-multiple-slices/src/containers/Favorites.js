ed54a7b8d2cdd0407c575f2a83d397da0f47acd8
NtekOskar <oskar.berntorp@novotek.com>
2021-10-28 11:41:14 +0200

    Changes made:
    - Done with coding section25,
    added example of using multiple slices with storeHook

--- UdemyReactUltimateCourceNew/section25/replace-redux-06-bonus-multiple-slices/src/containers/Favorites.js
+++ UdemyReactUltimateCourceNew/section25/replace-redux-06-bonus-multiple-slices/src/containers/Favorites.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useContext } from 'react';
 
-import FavoriteItem from "../components/Favorites/FavoriteItem";
-import { useStore } from "../hooks-store/store";
-import "./Products.css";
+import FavoriteItem from '../components/Favorites/FavoriteItem';
+import { useStore } from '../hooks-store/store';
+import './Products.css';
 
-const Favorites = (props) => {
+const Favorites = props => {
   const state = useStore()[0];
-  const favoriteProducts = state.products.filter((p) => p.isFavorite);
+  const favoriteProducts = state.products.filter(p => p.isFavorite);
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
       <ul className="products-list">
-        {favoriteProducts.map((prod) => (
+        {favoriteProducts.map(prod => (
           <FavoriteItem
             key={prod.id}
             id={prod.id}
