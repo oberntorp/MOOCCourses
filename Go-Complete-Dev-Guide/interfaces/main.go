7b241bcff7156ad70b9883e9cb6d5003aac17dd6
oberntorp <oberntorp@hotmail.com>
2021-12-27 12:42:30 +0100

    Changes made:
    - Begun to learn about interfaces in go, section 6
    started, 3 lectures done

---
+++ Go-Complete-Dev-Guide/interfaces/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type bot interface{
+	getGreeting() string
+}
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	var eb englishBot = englishBot{}
+	var sb spanishBot = spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+// If not using the receiver value, it can be omitted and just the type is needed
+func (eb englishBot) getGreeting() string {
+	return "Hi There!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (eb spanishBot) getGreeting() string {
+	return "Holla!"
+}
