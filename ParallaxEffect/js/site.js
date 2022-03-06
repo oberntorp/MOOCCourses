f2f2fe7a74877ab0064832ae627ee60bec2b7f29
oberntorp <oberntorp@hotmail.com>
2022-03-06 13:33:39 +0100

    Changes made:
    - Experimenting with parallax effect

---
+++ ParallaxEffect/js/site.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  const boat = document.querySelector("#boat");
+  boat.style.top = scrollY * 0.4 + "px";
+
+  const p = document.querySelector("p");
+  p.style.left = scrollY * 0.3 + "px";
+
+  const h1 = document.querySelector("#boat h1");
+  h1.style.top = scrollY * 0.1 + "px";
+  h1.style.left = scrollY * 0.1 + "px";
+});
